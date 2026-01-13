fa8da37b811978500947e427a7a3e2622dcbdc91
Cris575 <cg691539@gmial.com>
2026-01-13 14:08:00 -0600

    inital commit

---
+++ src/clean-code/02-name-types.ts
@@ -0,0 +1,34 @@
+(() => {
+
+    const temperaturesCelsius = [33.6, 12.34];
+
+    const serverIp = '123.123.123.123';
+
+    const listOfUsers = [{ id: 1, email: 'fernando@google.com' }, { id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+    const listOfUserEmails = listOfUsers.map(user => user.email);
+
+    const isJumping = false;
+    const isRunning = true;
+    const hasItems = true;
+    const isLoading = false;
+
+    const startTime = new Date().getTime();
+    const endTime = new Date().getTime() - startTime;
+
+
+    function getBooks() {
+        throw new Error('Function not implemented.');
+    }
+
+    function getBooksUrl(url: string) {
+        throw new Error('Function not implemented.');
+    }
+
+    function getSquareArea(side: number) {
+        throw new Error('Function not implemented.');
+    }
+    function printJob() {
+        throw new Error('Function not implemented.');
+    }
+
+})();
